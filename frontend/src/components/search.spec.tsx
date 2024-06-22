5add8a1830d9a77bfe473a798408ec03475ce4bd
facuperezm <facundoperezmontalvo@gmail.com>
2024-06-22 13:07:54 -0300

    feat: Add renderWithClient helper function for testing and update Search component tests [wip]

--- frontend/src/components/search.spec.tsx
+++ frontend/src/components/search.spec.tsx
@@ -1,26 +1,14 @@
-import { render, screen, waitFor } from "@testing-library/react";
-import { renderHook } from "@testing-library/react";
 import Search from "./search";
-import { QueryClientProvider, useQuery } from "@tanstack/react-query";
-import { queryClient } from "@/lib/react-query";
+import { renderWithClient } from "@/lib/helper";
 
-export function useCustomHook() {
-  return useQuery({ queryKey: ["customHook"], queryFn: () => "Hello" });
-}
-
-const createWrapper = () => {
-  return ({ children }: { children: React.ReactNode }) => (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  );
-};
-
-describe("SearchComponent", () => {
+describe("Search component", () => {
   it("should render correctly", async () => {
-    const { result } = renderHook(() => useCustomHook(), {
-      wrapper: createWrapper(),
-    });
-    await waitFor(() => {
-      expect(result.current.data).toBe("Hello");
-    });
+    renderWithClient(<Search />);
+
+    expect(/loading/i).toBeDefined();
+  });
+
+  it("should load the table correctly", () => {
+    renderWithClient(<Search />);
   });
 });
